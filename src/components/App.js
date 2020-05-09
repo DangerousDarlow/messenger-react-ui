bd29fc021cd9b54942e9cc9e5d6638756f58e242
Nick Darlow <dangerous.darlow@gmail.com>
2020-05-09 17:52:56 +0100

    add connect component template

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Connect from "./Connect";
 
 const App = () => {
-  return <div>My App</div>;
-}
+    return (
+        <div className="ui container">
+            <Connect />
+        </div>
+    );
+};
 
 export default App;
