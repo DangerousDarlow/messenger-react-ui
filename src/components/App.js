09ecca245ee801f283616d6c6e0ab1715abec008
Nick Darlow <dangerous.darlow@gmail.com>
2020-05-10 17:14:00 +0100

    layout connect, message and send components in app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Connect from "./Connect";
+import Messages from "./Messages";
+import Send from "./Send";
+import "./App.css";
 
 const App = () => {
     return (
-        <div className="ui container">
-            <Connect />
+        <div className="ui app">
+            <div className="ui header">
+                <div className="ui center aligned grid">
+                    <div className="column">
+                        <Connect />
+                    </div>
+                </div>
+            </div>
+            <div className="content">
+                <Messages />
+            </div>
+            <div className="footer">
+                <Send />
+            </div>
         </div>
     );
 };
--- src/components/Connect.js
+++ src/components/Connect.js
@@ -2,19 +2,12 @@ import React from "react";
 
 const Connect = () => {
     return (
-        <div
-            className="ui stackable center aligned grid"
-            style={{ marginTop: 4, marginBottom: 4 }}
-        >
-            <div className="column twelve wide">
-                <div className="ui action input">
-                    <input type="text" placeholder="your name" />
-                    <button className="ui button primary">
-                        <i className="sign in alternate icon" />
-                        Connect
-                    </button>
-                </div>
-            </div>
+        <div className="ui action input">
+            <input type="text" placeholder="your name" />
+            <button className="ui button primary">
+                <i className="sign in alternate icon" />
+                Connect
+            </button>
         </div>
     );
 };
