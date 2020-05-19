1ec1c9f900b5873f64331f076378492d0ecfcae0
Nick Darlow <dangerous.darlow@gmail.com>
2020-05-19 21:07:56 +0100

    replace app.css with styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,48 @@
 import React from "react";
+
+import { withStyles } from "@material-ui/core/styles";
+
 import Connect from "./Connect";
 import Messages from "./Messages";
 import Send from "./Send";
-import "./App.css";
 
-const App = () => {
+const styles = {
+    app: {
+        display: "flex",
+        height: "-webkit-fill-available",
+        flexDirection: "column",
+        flexWrap: "nowrap",
+        backgroundColor: "lightyellow",
+    },
+    header: {
+        flexShrink: 0,
+        backgroundColor: "lightsalmon",
+    },
+    content: {
+        flexGrow: 1,
+        overflow: "auto",
+        backgroundColor: "lightgreen",
+    },
+    footer: {
+        flexShrink: 0,
+        backgroundColor: "lightblue",
+    },
+};
+
+const App = ({ classes }) => {
     return (
-        <div className="app">
-            <div className="header">
+        <div className={classes.app}>
+            <div className={classes.header}>
                 <Connect />
             </div>
-            <div className="content">
+            <div className={classes.content}>
                 <Messages />
             </div>
-            <div className="footer">
+            <div className={classes.footer}>
                 <Send />
             </div>
         </div>
     );
 };
 
-export default App;
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+
 import * as serviceWorker from "./serviceWorker";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import "typeface-roboto";
+
+import "./index.css";
+import App from "./components/App";
 
 ReactDOM.render(
     <React.StrictMode>
